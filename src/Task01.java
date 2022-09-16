aaacacd0e669cc13d1f5e3c39b772160d21fe8ed
Milana-Gulik <milanagulik@mail.ru>
2022-09-16 13:08:52 +0300

    Did refactoring of Task01 with method

--- src/Task01.java
+++ src/Task01.java
@@ -1,29 +1,41 @@
-// break continue return throw
+import java.util.Scanner;
 
-    public class Task01 {
-        public static void main(String[] args) {
-            int number = 23545;
+public class Task01 {
 
-            int max = number % 10;
-            number /= 10;
-//        int max = 0;
+    public static int findMaxDigit (int number) {
+        number = number < 0 ? -number : number;
+//        number = Math.abs(number);
 
-            while (number != 0) {
-                int digit = number % 10;
+        int max = number % 10;
+        number /= 10;
 
-                if (digit == 9) {
-                    max = digit;
-                    break;
-                }
+        while (number != 0) {
+            int digit = number % 10;
 
-                if (digit > max) {
-                    max = digit;
-                }
-
-                number /= 10;
-
+            if (digit == 9) {
+                max = digit;
+                break;
             }
 
-            System.out.println("Max number digit is " + max);
+            if (digit > max) {
+                max = digit;
+            }
+
+            number /= 10;
+
+        }
+
+        return max;
+    }
+        public static void main(String[] args) {
+            Scanner scanner = new Scanner(System.in);
+
+            System.out.print("Input your number: ");
+            int number = scanner.nextInt();
+
+            int digit = findMaxDigit(number);
+
+
+            System.out.println("Max number digit is " + digit);
         }
     }
