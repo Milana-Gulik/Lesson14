a97d51c99ac72cd9821631afa5e73165cda0ca7d
Milana-Gulik <milanagulik@mail.ru>
2022-09-16 21:30:21 +0300

    Changed Task07 with palindrome task

--- src/Task07.java
+++ src/Task07.java
@@ -1,4 +1,34 @@
 public class Task07 {
 
-}
+    public static boolean checkPalindrome(int number) {
 
+        // fool proof ("защита от дурака")
+        if (number <= 0) {
+            return false;
+        }
+
+        int copy = number;
+        int num = 0;
+
+        while(copy > 9){
+            num += copy % 10;
+            num *= 10;
+            copy /= 10;
+        }
+
+        num += copy;
+
+        return num == number;
+    }
+
+    public static void main(String[] args) {
+        int number = Task02.inputPositiveNumber();
+
+        boolean result = checkPalindrome(number);
+
+        String msg = result ? "Yes, number is palindrome"
+                : "No, number isn't palindrome";
+
+        System.out.println(msg);
+    }
+}
