b24949cca004f58a4c31a9792ad8cbc490009599
Milana-Gulik <milanagulik@mail.ru>
2022-09-16 13:46:27 +0300

    Did refactoring of Task03 with static method

--- src/Task03.java
+++ src/Task03.java
@@ -1,14 +1,26 @@
+import java.util.Scanner;
+
 // output only even (чётные) number
 // with continue operator
 public class Task03 {
-    public static void main(String[] args) {
-        int number = 100;
 
+    public static String getAllEvenNumbers(int number) {
+        String msg = " ";
         for (int i = 1; i < number; i++) {
             if (i % 2 == 1) {
                 continue;
             }
-            System.out.print(i + " ");
+            msg += i + " ";
         }
+
+        return msg;
+    }
+    public static void main(String[] args) {
+
+        int number = Task02.inputPositiveNumber();
+
+        String result = getAllEvenNumbers(number);
+        System.out.println(result);
+
     }
 }
